b7f8bfbd6d1603bb469be537b13438de500b22dd
Jonathan Wilson <jon.wilsond@gmail.com>
2018-05-24 09:38:25 -0400

    Updated 5/24

--- SQLQuery10.sql
+++ SQLQuery10.sql
@@ -1,3 +1,5 @@
+Drop table [previouscars]
+
 Create table PreviousCars 
 (
 	Id int not null identity(1,1) primary key,
@@ -6,10 +8,16 @@ Create table PreviousCars
 	Trim nvarchar(15),
 	Year int not null,
 	IsSedan bit not null default 1,
-	PurchasePrice Decimal(5,2) not null
+	PurchasePrice Decimal(7,2) not null
 );
 
 go
 
-insert into PreviousCars
-	(Make, Model, Trim, 
+insert into [PreviousCars]
+	(Make, Model, Trim, Year, IsSedan, PurchasePrice)
+Values
+	('Ford', 'Expidition', 'XL',2000,0,40000),
+	('Nissan','Altima', 'XLS',2002,1,10452.36),
+	('Toyota','Avalon','XL',2004,1,4563.78)
+
+Select * from [PreviousCars]
