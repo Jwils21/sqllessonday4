a2685a2dce29d002a1bda0ae1e8625911020202d
Jonathan Wilson <jon.wilsond@gmail.com>
2018-05-24 16:06:40 -0400

    Procedures for 5/24

---
+++ StoredProcedure.sql
@@ -0,0 +1,13 @@
+Alter Procedure QueryOrders
+	@minamt int = 500,
+	@maxamt int = null
+as 
+Begin
+	case
+		when @maxamt is null then Select * from [order] where amount > @minamt
+		Else Select * from [order] where amount between @minamt and @maxamt
+	End
+End
+go
+exec QueryOrders 500;
+gos
